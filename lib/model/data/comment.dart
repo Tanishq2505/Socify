5ed6840ae6a19838f32fd6042ee7e514b06dc44f
Tanishq2505 <tanishqa89@gmail.com>
2023-02-22 21:44:03 +0530

    comment model and repository files 🔧

---
+++ lib/model/data/comment.dart
@@ -0,0 +1,63 @@
+import 'package:socify/model/data/user.dart';
+
+class CommentListData {
+  List<CommentData>? data;
+  int? total;
+  int? page;
+  int? limit;
+
+  CommentListData({this.data, this.total, this.page, this.limit});
+
+  CommentListData.fromJson(Map<String, dynamic> json) {
+    if (json['data'] != null) {
+      data = <CommentData>[];
+      json['data'].forEach((v) {
+        data!.add(new CommentData.fromJson(v));
+      });
+    }
+    total = json['total'];
+    page = json['page'];
+    limit = json['limit'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    data['total'] = this.total;
+    data['page'] = this.page;
+    data['limit'] = this.limit;
+    return data;
+  }
+}
+
+class CommentData {
+  String? id;
+  String? message;
+  UserData? owner;
+  String? post;
+  String? publishDate;
+
+  CommentData({this.id, this.message, this.owner, this.post, this.publishDate});
+
+  CommentData.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    message = json['message'];
+    owner = json['owner'] != null ? new UserData.fromJson(json['owner']) : null;
+    post = json['post'];
+    publishDate = json['publishDate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['message'] = this.message;
+    if (this.owner != null) {
+      data['owner'] = this.owner!.toJson();
+    }
+    data['post'] = this.post;
+    data['publishDate'] = this.publishDate;
+    return data;
+  }
+}
---
+++ lib/view_model/respositories/comment_repository.dart
@@ -0,0 +1,97 @@
+import 'package:socify/model/data/comment.dart';
+import 'package:socify/model/services/media_service.dart';
+import 'package:flutter/material.dart';
+import 'package:socify/view/widgets/show_snackbar.dart';
+
+class CommentRepositories {
+  late final MediaService _mediaService;
+  late BuildContext _context;
+  CommentRepositories(BuildContext context) {
+    _mediaService = MediaService();
+    _context = context;
+  }
+
+  Future<CommentListData?> getCommentList({required String page}) async {
+    try {
+      CommentListData data = CommentListData.fromJson(
+        await _mediaService.get("/comment?page=$page&limit=10"),
+      );
+      return data;
+    } catch (e) {
+      showSnackbar(
+        _context,
+        e.toString(),
+        Colors.red,
+      );
+    }
+  }
+
+  Future<CommentListData?> getCommentListByPost(
+      {required String id, required String page}) async {
+    try {
+      CommentListData data = CommentListData.fromJson(
+        await _mediaService.get("/post/$id/comment?page=$page&limit=10"),
+      );
+      return data;
+    } catch (e) {
+      showSnackbar(
+        _context,
+        e.toString(),
+        Colors.red,
+      );
+    }
+  }
+
+  Future<CommentListData?> getCommentListByUser(
+      {required String id, required String page}) async {
+    try {
+      CommentListData data = CommentListData.fromJson(
+        await _mediaService.get("/user/$id/comment?page=$page&limit=10"),
+      );
+      return data;
+    } catch (e) {
+      showSnackbar(
+        _context,
+        e.toString(),
+        Colors.red,
+      );
+    }
+  }
+
+  Future<CommentData?> createComment({required CommentData commentData}) async {
+    try {
+      CommentData data = CommentData.fromJson(
+        await _mediaService.post(
+          "/post/create",
+          commentData.toJson(),
+        ),
+      );
+      return data;
+    } catch (e) {
+      showSnackbar(
+        _context,
+        e.toString(),
+        Colors.red,
+      );
+    }
+  }
+
+  Future<dynamic> deleteUser({required CommentData commentData}) async {
+    try {
+      CommentData data = CommentData.fromJson(
+        await _mediaService.delete("/comment/${commentData.id}"),
+      );
+      if (commentData.id == data.id) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      showSnackbar(
+        _context,
+        e.toString(),
+        Colors.red,
+      );
+    }
+  }
+}
